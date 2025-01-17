d8e0d9fe3e3fb6f8ac8695e85bf4e790015d0ed1
transjenny <134710445+transjenny@users.noreply.github.com>
2025-01-17 13:12:03 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+function OnLoad()
+{
+    console.log("Starting Document init");
+    var WelcomeText = document.getElementById("WelcomeText");
+    var BasicInfo = document.getElementById("BasicInfo");
+    var BufferTitle = document.getElementById("bufferOverTitle");
+    BufferTitle.innerHTML = WelcomeText.innerHTML;
+    console.log(WelcomeText.style.fontsize);
+    WelcomeText.style.fontsize = "60px";
+    document.getElementById("bufferOverTitle").style.fontSize = "80px"
+    BufferTitle.style.opacity = "0";
+    var WindowWidth = window.innerWidth;
+    var WindowHeight = window.innerHeight;
+    WelcomeText.style.top = "10px";
+    WelcomeText.style.left = "25%";
+    
+    
+    
+}
+
+function OnResize()
+{
+    console.log("resizing window");
+    OnLoad();   
+}
+document.addEventListener("resize", OnResize);
+OnLoad();
